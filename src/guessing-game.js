571439e96607b64f8f99f521967c2d49ee70ac8c
eisenchamp <105707349+eisenchamp@users.noreply.github.com>
2022-08-16 14:19:30 +0600

    doc: update code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
+        this.half = Math.round((this.maxValue + this.minValue) / 2)
 
+        let randomNumber = Math.floor(Math.random() * (this.maxValue - this.minValue) + this.minValue);
+
+        if (this.half < this.number){
+            this.greater();
+        } else if (this.half > this.number){
+            this.lower();
+        }
+        randomNumber = Math.ceil((this.maxValue + this.minValue) / 2);
+        return randomNumber;
     }
 
     lower() {
-
+        this.maxValue = Math.round((this.maxValue + this.minValue) / 2);
     }
 
     greater() {
-
+        this.minValue = Math.round((this.maxValue + this.minValue) / 2);
     }
 }
 
